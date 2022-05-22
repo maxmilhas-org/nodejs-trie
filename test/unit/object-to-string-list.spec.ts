704a4ac07eb1b8bc7ca11b60c360ab18132511aa
Thiago Santos <tos.oliveira@gmail.com>
2022-05-22 11:48:55 -0300

    test: fixing sonar crying

--- test/unit/object-to-string-list.spec.ts
+++ test/unit/object-to-string-list.spec.ts
@@ -1,36 +1,35 @@
 import { objectToStringList } from '../../src/object-to-string-list';
 
 describe(objectToStringList.name, () => {
-	it('Should process an object', () => {
-		const iterable = objectToStringList(
+	const baseObject = {
+		field1: 1,
+		field2: true,
+		field3: 'some words',
+		field4: [
+			'more words',
 			{
-				field1: 1,
-				field2: true,
-				field3: 'some words',
-				field4: [
-					'more words',
-					{
-						field5: null,
-						field6: 'even more',
-						field7: ['********$%45', 'codE 779kk0'],
-						field8: {
-							field9: "That's awkward",
-							field10: false,
-							field11: "Isn't it? Demodé",
-						},
-					},
-				],
-				field12: {
-					field13: Number.POSITIVE_INFINITY,
-					field14: 'Almost done',
-					field15: undefined,
+				field5: null,
+				field6: 'even more',
+				field7: ['********$%45', 'codE 779kk0'],
+				field8: {
+					field9: "That's awkward",
+					field10: false,
+					field11: "Isn't it? Demodé",
 				},
 			},
-			{
-				minSize: 4,
-				forbiddenWords: ['code'],
-			},
-		);
+		],
+		field12: {
+			field13: Number.POSITIVE_INFINITY,
+			field14: 'Almost done',
+			field15: undefined,
+		},
+	};
+
+	it('Should process an object', () => {
+		const iterable = objectToStringList(baseObject, {
+			minSize: 4,
+			forbiddenWords: ['code'],
+		});
 		const result = Array.from(iterable);
 
 		expect(result).toEqual([
@@ -50,37 +49,12 @@ describe(objectToStringList.name, () => {
 	});
 
 	it('Should process an object when using insensitive strings', () => {
-		const iterable = objectToStringList(
-			{
-				field1: 1,
-				field2: true,
-				field3: 'some words',
-				field4: [
-					'more words',
-					{
-						field5: null,
-						field6: 'even more',
-						field7: ['********$%45', 'codE 779kk0'],
-						field8: {
-							field9: "That's awkward",
-							field10: false,
-							field11: "Isn't it? Demodé",
-						},
-					},
-				],
-				field12: {
-					field13: Number.POSITIVE_INFINITY,
-					field14: 'Almost done',
-					field15: undefined,
-				},
-			},
-			{
-				minSize: 4,
-				forbiddenWords: ['code'],
-				onlyAlphaNumerics: true,
-				caseInsensitive: true,
-			},
-		);
+		const iterable = objectToStringList(baseObject, {
+			minSize: 4,
+			forbiddenWords: ['code'],
+			onlyAlphaNumerics: true,
+			caseInsensitive: true,
+		});
 		const result = Array.from(iterable);
 
 		expect(result).toEqual([
